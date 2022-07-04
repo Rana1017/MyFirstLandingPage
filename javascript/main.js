531d72193efed248f5f9c0c00c95749ab220c9d4
Top Rana <toprana212@gmail.com>
2022-07-04 21:31:19 +0545

    Added Scroll down to bottom function in Js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.menu')
 const closed = document.querySelector('.closed')
 const nav = document.querySelector('nav')
+const arrow = document.querySelector('.arrow-svg')
 
 menu.addEventListener('click',()=>{
     nav.classList.add('open-nav')
@@ -8,4 +9,8 @@ menu.addEventListener('click',()=>{
 
 closed.addEventListener('click',()=>{
     nav.classList.remove('open-nav')
+})
+
+arrow.addEventListener('click',()=>{
+    window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
 })
